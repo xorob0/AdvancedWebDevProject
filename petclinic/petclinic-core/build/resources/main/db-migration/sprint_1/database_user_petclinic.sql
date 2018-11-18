fe8ee6d39c3203ea98d8a604fc00c16a63f75b7e
Timothee Simon <xorob0@posteo.net>
2018-11-18 14:38:57 +0100

    Updtated gitignore

--- petclinic/petclinic-core/build/resources/main/db-migration/sprint_1/database_user_petclinic.sql
+++
@@ -1,6 +0,0 @@
---liquibase formatted sql
-
---changeset tricarico:1
-
-CREATE DATABASE petclinic CHARACTER SET utf8 COLLATE utf8_general_ci;
-GRANT SELECT, INSERT, UPDATE, DELETE ON petclinic.* to 'petclinic'@'%' IDENTIFIED BY 'petclinic';
--- petclinic/petclinic-core/build/resources/main/db-migration/sprint_1/populate_vets.sql
+++
@@ -1,9 +0,0 @@
---liquibase formatted sql
-
---changeset tricarico:3
-USE petclinic;
-
-INSERT INTO vets (first_name,last_name,speciality) VALUES ('James','Carter','radiology');
-INSERT INTO vets (first_name,last_name,speciality) VALUES ('Helen','Leary','surgery');
-INSERT INTO vets (first_name,last_name,speciality) VALUES ('Linda','Douglas','radiology');
-INSERT INTO vets (first_name,last_name,speciality) VALUES ('Rafael','Ortega','dentistry');
